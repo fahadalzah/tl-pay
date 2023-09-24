a85c7e639c8ad2a5541063cc0f381cb60f32a35c
Saud Alfaraj <saud.alfaraj15@icloud.com>
2023-09-24 15:45:15 +0300

    edit home page and price

--- pages/prices/prices.js
+++ pages/prices/prices.js
@@ -46,3 +46,17 @@ month1.addEventListener('click', function () {
     }
     body.appendChild(month1section)
 })
+
+
+document.addEventListener('scroll', function () {
+const nav = document.querySelector('.nav');
+// كود أذا نزل بل الماوس إلى هاذي القيمة يقوم بالأوامر في الأسفل
+if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+    // يظهر تنسيق السكرولد ويكون فيه تغير خلفية الناف بار
+}    else {
+    // إزالة التنسيق
+    nav.classList.remove('scrolled');
+
+}
+})
